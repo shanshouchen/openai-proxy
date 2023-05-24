f4cceb1cab79da177e411cea525902e9c8f9e7dc
fencer <982039620@qq.com>
2023-05-24 10:17:48 +0800

    add cross

--- main.ts
+++ main.ts
@@ -1,21 +1,17 @@
 import {serve} from "https://deno.land/std@0.181.0/http/server.ts";
+import { cors_all } from "https://deno.land/x/masx200_deno_http_middleware@1.0.2/middleware.ts";
+import { createHandler } from "https://deno.land/x/masx200_deno_http_middleware@1.2.1/mod.ts";
 
 const OPENAI_API_HOST = "api.openai.com";
 
-serve(async (request) => {
-    const url = new URL(request.url);
-
+const handler = createHandler([cors_all, async (ctx) => {
+    const url = new URL(ctx.request.url);
     if (url.pathname === "/") {
         return fetch(new URL("./desc.md", import.meta.url));
     }
 
     url.host = OPENAI_API_HOST;
-    request.respond({
-        headers: new Headers({
-            'Access-Control-Allow-Origin': '*', // 允许所有来源的请求
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE', // 允许的请求方法
-            'Access-Control-Allow-Headers': 'Content-Type', // 允许的请求头
-        }),
-    });
-    return await fetch(url, request);
-});
+    return await fetch(url, ctx.request);
+}])
+
+serve(handler);
